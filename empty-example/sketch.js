15385e5086a96349eac571b9c608f15975454e51
niC00L <nika.blanarova@gmail.com>
2018-06-02 18:18:30 +0200

    Polygon voronoi

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,51 +1,37 @@
-let vw = $(window).width();
+let vw = $(window).width() - 300;
 let vh = $(window).height();
+let voronoi = d3.voronoi();
 
 function setup() {
     createCanvas(vw, vh);
-    console.log(vw, vh);
     noLoop();
 }
 
 function draw() {
-    points = getPoints(10);
-    noStroke();
-    let d = pixelDensity();
-    let i;
-    let j;
-    for (i = 1; i <= vw * d; i++) {
-        for (j = 1; j <= vh * d; j++) {
-            c = closestPoint({x: i, y: j}, points).color;
-            set(i, j, c);
-        }
-    }
-    updatePixels();
+    let points = getPoints(random(10, 50));
+    voronoi.extent([[0, 0], [vw, vh]]);
+    let vp = voronoi(points).polygons();
+    vp.forEach(function (p) {
+        push();
+        fill(random(255), random(255), random(255));
+        polygon(p);
+        pop();
+    });
 }
 
 function getPoints(count) {
     let points = [];
     let i;
     for (i = 0; i <= count; i++) {
-        points.push({x: random(0, vw), y: random(0, vh), color: color(random(0, 255), random(0, 255), random(0, 255))});
+        points.push([random(0, vw), random(0, vh)]);
     }
     return points;
 }
 
-function distance(point1, point2) {
-    a = abs(point1.x - point2.x);
-    b = abs(point1.y - point2.y);
-    return Math.sqrt(a * a + b * b);
-}
-
-function closestPoint(point, points) {
-    let closest = points[0];
-    let d = distance(point, closest);
+function polygon(points) {
+    beginShape();
     points.forEach(function (p) {
-        var newD = distance(point, p);
-        if (newD < d) {
-            closest = p;
-            d = newD;
-        }
+        vertex(p[0], p[1]);
     });
-    return closest;
+    endShape(CLOSE);
 }
